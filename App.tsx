f9e5d3b40fcfec8235b47cac5fa62aef1383a2ed
jihongeek <jihongeek@kakao.com>
2026-04-09 10:07:29 +0900

    chore: 지원 종료된 `SafeAreaView` 대체를 위한 의존성 설치 및 적용

--- App.tsx
+++ App.tsx
@@ -1,12 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
-import {
-  StyleSheet,
-  View,
-  BackHandler,
-  Platform,
-  SafeAreaView,
-  Text,
-} from 'react-native';
+import { StyleSheet, View, BackHandler, Platform, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { WebView, WebViewNavigation } from 'react-native-webview';
 import { StatusBar } from 'expo-status-bar';
 import * as Linking from 'expo-linking';
